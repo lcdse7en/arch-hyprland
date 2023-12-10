aa5898a19d3f8fa34479883dd90e590bf4c38466
lcdse7en <2353442022@qq.com>
2023-12-10 19:06:25 +0800

    add

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+#********************************************
+# Author      : lcdse7en                    *
+# E-mail      : 2353442022@qq.com           *
+# Create_Time : 2023-12-10 19:05:15         *
+# Description : Install hyprland script     *
+#********************************************
